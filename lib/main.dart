fc51e790fadf940c67885bbc7913c549b316fd6f
Htue Ko <htooko1101@gmail.com>
2021-08-04 19:15:43 +0630

    update main to start the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'common/route/app_page.dart';
+import 'common/route/app_route.dart';
+import 'feature/get_countries/di/connectivity_binding.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialBinding: ConnectivityBinding(),
+      initialRoute: AppRoutes.countriesRoute,
+      getPages: AppPages.list,
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+    );
   }
 }
